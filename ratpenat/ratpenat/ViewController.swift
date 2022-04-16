a2e1c80f49268943c40e67e4f0770790353a4fbe
Jose A Lobato <21879+josealobato@users.noreply.github.com>
2022-04-16 17:20:45 +0200

    basic app with UIKit but without storyboards
    issue #1

---
+++ ratpenat/ratpenat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ratpenat
+//
+//  Created by Jose Lobato on 16/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
